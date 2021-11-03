ca7ab648340db2566492368ff2be641e4bf4d378
akirsteina <anna.kirsteina@gmail.com>
2021-11-03 15:53:11 +0200

    filter expenses by year

--- expenses-tracking/src/components/Expenses/Expenses.js
+++ expenses-tracking/src/components/Expenses/Expenses.js
@@ -13,13 +13,24 @@ const Expenses = (props) => {
 		setFilteredYear(selectedYear);
 	};
 
+	const filteredExpenses = props.expenses.filter(
+		(expense) => {
+			return expense.date.getFullYear().toString() === filteredYear;
+		}
+		// (expense.date.toString().indexOf(filteredYear) !== -1)
+	);
+
+	let expensesContent = <p>No expenses found</p>;
+	if (filteredExpenses.length > 0) {
+		expensesContent = filteredExpenses.map((expense) => (
+			<ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} key={expense.id} />
+		));
+	}
+
 	return (
 		<Card className='expenses'>
 			<ExpensesFilter selectedYear={filteredYear} onChangeFilter={filterChangeHandler} />
-
-			{props.expenses.map((expense, index) => (
-				<ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} key={index} />
-			))}
+			{expensesContent}
 		</Card>
 	);
 };
