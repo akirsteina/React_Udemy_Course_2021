66d579a740f0082451a958c28dff5d94e3a33c89
akirsteina <anna.kirsteina@gmail.com>
2021-11-04 12:09:52 +0200

    update app component

--- add-new-user/src/App.js
+++ add-new-user/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-
+import AddNewUser from './components/Users/AddNewUser';
+import UsersList from './components/Users/UsersList';
+import userData from './data/users.json';
 
 function App() {
-  return (
-    <div>
-
-    </div>
-  );
+	return (
+		<div>
+			<AddNewUser />
+			<UsersList users={userData} />
+		</div>
+	);
 }
 
 export default App;
