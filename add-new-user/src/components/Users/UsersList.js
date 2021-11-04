35648e095a5bfdbe1fc9b80f2b2374f7dbf43fd9
akirsteina <anna.kirsteina@gmail.com>
2021-11-04 14:32:44 +0200

    add card element

--- add-new-user/src/components/Users/UsersList.js
+++ add-new-user/src/components/Users/UsersList.js
@@ -1,13 +1,16 @@
 import UserItem from './UserItem';
+import Card from '../UI/Card';
 import style from '../../assets/UsersList.module.css';
 
 const UsersList = (props) => {
 	return (
-		<ul className={style.users}>
-			{props.users.map((user) => (
-				<UserItem username={user.username} age={user.userAge} key={user.userId} />
-			))}
-		</ul>
+		<Card>
+			<ul className={style.users}>
+				{props.users.map((user) => (
+					<UserItem username={user.username} age={user.userAge} key={user.userId} />
+				))}
+			</ul>
+		</Card>
 	);
 };
 export default UsersList;
