402d0ab9bca5c1724b7daae0dd4b84fae4010788
akirsteina <anna.kirsteina@gmail.com>
2021-11-05 12:26:54 +0200

    add useReducer training

--- dummy-login-page-useEffect-training/src/components/Login/Login.js
+++ dummy-login-page-useEffect-training/src/components/Login/Login.js
@@ -12,7 +12,12 @@ const Login = (props) => {
 	const [formIsValid, setFormIsValid] = useState(false);
 
 	useEffect(() => {
-		setFormIsValid(enteredEmail.includes('@') && enteredPassword.trim().length > 6);
+		const identifier = setTimeout(() => {
+			setFormIsValid(enteredEmail.includes('@') && enteredPassword.trim().length > 6);
+		}, 500);
+		return () => {
+			clearTimeout(identifier);
+		}; //cleanup function
 	}, [enteredEmail, enteredPassword]);
 
 	const emailChangeHandler = (event) => {
