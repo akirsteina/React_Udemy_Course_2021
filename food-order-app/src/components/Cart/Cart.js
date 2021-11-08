31f48f3b5bde68f4b2c1d42e3b59148dc31796e6
akirsteina <anna.kirsteina@gmail.com>
2021-11-08 12:52:19 +0200

    update add to cart and remove to cart functionality

--- food-order-app/src/components/Cart/Cart.js
+++ food-order-app/src/components/Cart/Cart.js
@@ -1,6 +1,7 @@
 import { useContext } from 'react';
 
 import Modal from '../UI/Modal';
+import CartItem from './CartItem';
 import CartContext from '../../store/cart-context';
 
 import styles from '../../assets/Cart.module.css';
@@ -11,10 +12,25 @@ const Cart = (props) => {
 	const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
 	const hasItems = cartCtx.items.length > 0;
 
+	const cartItemRemoveHandler = (id) => {
+		cartCtx.removeItem(id);
+	};
+
+	const cartItemAddHandler = (item) => {
+		cartCtx.addItem({ ...item, amount: 1 });
+	};
+
 	const cartItems = (
-		<ul className={styles['card-items']}>
-			{cartCtx.items.map((cartItem) => (
-				<li>{cartItem.name}</li>
+		<ul className={styles['cart-items']}>
+			{cartCtx.items.map((item) => (
+				<CartItem
+					key={item.id}
+					name={item.name}
+					amount={item.amount}
+					price={item.price}
+					onRemove={cartItemRemoveHandler.bind(null, item.id)}
+					onAdd={cartItemAddHandler.bind(null, item)}
+				></CartItem>
 			))}
 		</ul>
 	);
--- food-order-app/src/store/CartProvider.js
+++ food-order-app/src/store/CartProvider.js
@@ -10,14 +10,46 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
 	if (action.type === 'ADD_CART_ITEM') {
-		const updatedItems = state.items.concat(action.item); // like push, but creates new array
 		const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+
+		const existingCartItemIndex = state.items.findIndex((item) => item.id === action.item.id);
+
+		const existingCartItem = state.items[existingCartItemIndex];
+
+		let updatedItems;
+		if (existingCartItem) {
+			const updatedItem = {
+				...existingCartItem,
+				amount: existingCartItem.amount + action.item.amount,
+			};
+			updatedItems = [...state.items];
+			updatedItems[existingCartItemIndex] = updatedItem;
+		} else {
+			updatedItems = state.items.concat(action.item); // like push, but creates new array
+		}
+
 		return {
 			items: updatedItems,
 			totalAmount: updatedTotalAmount,
 		};
 	}
+
 	if (action.type === 'REMOVE_CART_ITEM') {
+		const existingCartItemIndex = state.items.findIndex((item) => item.id === action.id);
+		const existingCartItem = state.items[existingCartItemIndex];
+		const updatedTotalAmount = state.totalAmount - existingCartItem.price;
+		let updatedItems;
+		if (existingCartItem.amount === 1) {
+			updatedItems = state.items.filter((item) => item.id !== action.id);
+		} else {
+			const updatedItem = { ...existingCartItem, amount: existingCartItem.amount - 1 };
+			updatedItems = [...state.items];
+			updatedItems[existingCartItemIndex] = updatedItem;
+		}
+		return {
+			items: updatedItems,
+			totalAmount: updatedTotalAmount,
+		};
 	}
 	return defaultCartState; //returns a new state snapshot
 };
