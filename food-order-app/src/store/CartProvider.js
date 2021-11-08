60c7b3d15ecbf468e8de13d0b92983bd7a6be6c6
akirsteina <anna.kirsteina@gmail.com>
2021-11-08 10:59:13 +0200

    update usereducer logic

--- food-order-app/src/store/CartProvider.js
+++ food-order-app/src/store/CartProvider.js
@@ -1,14 +1,49 @@
 import { useReducer } from 'react';
+// same as useState, but for more complex use cases
+
 import CartContext from './cart-context';
 
+const defaultCartState = {
+	items: [],
+	totalAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+	if (action === 'ADD_CART_ITEM') {
+		const updatedItems = state.items.concat(action.item); // like push, but creates new array
+		const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+		return {
+			items: updatedItems,
+			totalAmount: updatedTotalAmount,
+		};
+	}
+	if (action === 'REMOVE_CART_ITEM') {
+	}
+	return defaultCartState; //returns a new state snapshot
+};
+
 const CartProvider = (props) => {
-	const addItemToCartHandler = (item) => {};
-	const removeItemFromCartHandler = (id) => {};
+	// first element - state snapshot
+	// second element - function that allows to dispatch an action to reducer
+	const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
+
+	const addItemToCartHandler = (item) => {
+		dispatchCartAction({
+			type: 'ADD_CART_ITEM',
+			item: item,
+		});
+	};
+	const removeItemFromCartHandler = (id) => {
+		dispatchCartAction({
+			type: 'REMOVE_CART_ITEM',
+			id: id,
+		});
+	};
 
 	// dynamic, will change
 	const cartContext = {
-		items: [],
-		totalAmount: 0,
+		items: cartState.items,
+		totalAmount: cartState.totalAmount,
 		addItem: addItemToCartHandler,
 		removeItem: removeItemFromCartHandler,
 	};
