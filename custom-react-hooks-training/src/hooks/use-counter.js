de5cf0f7888109381382cad878bd08ac0279d794
akirsteina <anna.kirsteina@gmail.com>
2021-11-09 17:02:02 +0200

    add custom counter hook to remove similar functions

---
+++ custom-react-hooks-training/src/hooks/use-counter.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react';
+
+// function name for custom hook !!! has to start with 'use'
+const useCounter = (forwards = true) => {
+	const [counter, setCounter] = useState(0);
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			if (forwards) {
+				setCounter((prevCounter) => prevCounter + 1);
+			} else {
+				setCounter((prevCounter) => prevCounter - 1);
+			}
+		}, 1000);
+
+		return () => clearInterval(interval);
+	}, []);
+
+	return counter;
+};
+
+export default useCounter;
+
+// only the logic is shared, not the concrete state
