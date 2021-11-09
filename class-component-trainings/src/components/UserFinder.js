271c424dc22035e4f33347093cdbf19f7ef7054b
akirsteina <anna.kirsteina@gmail.com>
2021-11-09 14:19:59 +0200

    add context

--- class-component-trainings/src/components/UserFinder.js
+++ class-component-trainings/src/components/UserFinder.js
@@ -3,32 +3,28 @@ import { Fragment, Component } from 'react';
 import Users from './Users';
 
 import classes from './UserFinder.module.css';
-
-const DUMMY_USERS = [
-	{ id: 'u1', name: 'Max' },
-	{ id: 'u2', name: 'Manuel' },
-	{ id: 'u3', name: 'Julie' },
-];
+import UsersContext from '../store/users-context';
 
 class UserFinder extends Component {
+	static contextType = UsersContext;
 	constructor() {
 		super();
 		this.state = {
-			filteredUsers: DUMMY_USERS,
+			filteredUsers: [],
 			searchTerm: '',
 		};
 	}
 
 	componentDidMount() {
 		//sent http request...
-		this.setState({ filteredUsers: DUMMY_USERS });
+		this.setState({ filteredUsers: this.context.users });
 		// useEffect(() => {}, []) when the component is initally mounted
 	}
 
 	componentDidUpdate(prevProps, prevState) {
 		if (prevState.searchTerm !== this.state.searchTerm) {
 			this.setState({
-				filteredUsers: DUMMY_USERS.filter((user) => user.name.includes(this.state.searchTerm)),
+				filteredUsers: this.context.users.filter((user) => user.name.includes(this.state.searchTerm)),
 			});
 		}
 	}
