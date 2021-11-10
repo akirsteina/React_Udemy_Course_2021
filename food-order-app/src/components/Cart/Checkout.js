249b34931dc308c2493d5588a42ff3878178fc15
akirsteina <anna.kirsteina@gmail.com>
2021-11-10 17:58:07 +0200

    add basic validation for name input

--- food-order-app/src/components/Cart/Checkout.js
+++ food-order-app/src/components/Cart/Checkout.js
@@ -1,15 +1,58 @@
+import { useState } from 'react/cjs/react.development';
 import styles from '../../assets/Checkout.module.css';
 
 const CheckOut = (props) => {
+	const [enteredName, setEnteredName] = useState('');
+	const [nameInputIsValid, setNameInputIsValid] = useState(true);
+	const [nameInputTouched, setNameInputTouched] = useState(false);
+	// const [nameInputError, setNameInputError] = useState(null);
+
+	// let formIsValid = false;
+	// if (nameInputIsValid) {
+	// 	formIsValid = true;
+	// }
+
+	const enteredNameHandler = (event) => {
+		setEnteredName(event.target.value);
+		if (event.target.value.trim() !== '') {
+			setNameInputIsValid(true);
+			return;
+		}
+	};
+
+	const nameBlurHandler = () => {
+		setNameInputTouched(true);
+		if (enteredName.trim() === '') {
+			setNameInputIsValid(false);
+			return;
+		}
+	};
+
 	const checkoutSubmitHandler = (event) => {
 		event.preventDefault();
+
+		setNameInputTouched(true);
+
+		if (enteredName.trim() === '') {
+			setNameInputIsValid(false);
+			return;
+		}
+		setNameInputIsValid(true);
+		console.log('Submitted: ', enteredName);
+
+		setEnteredName('');
 	};
+
+	const nameInputIsInvalid = !nameInputIsValid && nameInputTouched;
+
+	const nameInputClasses = nameInputIsInvalid ? `${styles.control} ${styles.invalid}` : `${styles.control}`;
 
 	return (
 		<form onSubmit={checkoutSubmitHandler}>
-			<div className={styles.control}>
+			<div className={nameInputClasses}>
 				<label htmlFor='name'>Your name</label>
-				<input type='text' id='name' />
+				<input type='text' id='name' onChange={enteredNameHandler} onBlur={nameBlurHandler} value={enteredName} />
+				{nameInputIsInvalid && <p className={styles.error}>Please enter your name!</p>}
 			</div>
 			<div className={styles.control}>
 				<label htmlFor='street'>Street</label>
@@ -23,10 +66,12 @@ const CheckOut = (props) => {
 				<label htmlFor='city'>City</label>
 				<input type='text' id='city' />
 			</div>
-			<button type='button' onClick={props.onCancel}>
-				Cancel
-			</button>
-			<button type='submit'>Confirm</button>
+			<div className={styles.actions}>
+				<button type='button' onClick={props.onCancel}>
+					Cancel
+				</button>
+				<button type='submit'>Confirm</button>
+			</div>
 		</form>
 	);
 };
