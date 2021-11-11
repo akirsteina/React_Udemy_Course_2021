ef714e84923b7da5a9b3d05f16d4b93c02b0e0fc
akirsteina <anna.kirsteina@gmail.com>
2021-11-11 11:56:25 +0200

    add redux store for counter and provider in app.js

--- redux-counter/src/index.js
+++ redux-counter/src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
 import './index.css';
 import App from './App';
+import store from './store/index';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// provider should be added at the highest level needed as it can only work on children not parent components
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
