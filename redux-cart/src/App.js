65e291be6d0a7c9455716d5f71a8020c0b13162e
akirsteina <anna.kirsteina@gmail.com>
2021-11-19 16:49:55 +0200

    add fetch logic

--- redux-cart/src/App.js
+++ redux-cart/src/App.js
@@ -1,27 +1,80 @@
-import { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { Fragment, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { uiActions } from './store/ui-slice';
 
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import Notification from './components/UI/Notification';
+
+let isInitial = true;
+// will only be rendered the first time - when the file is first loaded and the app is started, but not when app re-renders
 
 function App() {
-	const cartIsVisible = useSelector((state) => state.ui.showCart);
+	const dispatch = useDispatch();
 
+	const cartIsVisible = useSelector((state) => state.ui.showCart);
 	const cart = useSelector((state) => state.cart);
+	const notification = useSelector((state) => state.ui.notification);
 
 	useEffect(() => {
-		fetch(
-			'https://react-http-25c2a-default-rtdb.europe-west1.firebasedatabase.app/cart.json',
-			{ method: 'PUT', body: JSON.stringify(cart) }
-		);
-	}, [cart]);
+		const sendCartData = async () => {
+			dispatch(
+				uiActions.showNotification({
+					status: 'pending',
+					title: 'Sending...',
+					message: 'Sending cart data!',
+				})
+			);
+			const response = await fetch(
+				'https://react-http-25c2a-default-rtdb.europe-west1.firebasedatabase.app/cart.json',
+				{ method: 'PUT', body: JSON.stringify(cart) }
+			);
+
+			if (!response.ok) {
+				throw new Error('Sending card data failed.');
+			}
+
+			dispatch(
+				uiActions.showNotification({
+					status: 'success',
+					title: 'Success!',
+					message: 'Sent cart data successfully!',
+				})
+			);
+		};
+
+		if (isInitial) {
+			isInitial = false;
+			return;
+		}
+
+		sendCartData().catch((error) => {
+			dispatch(
+				uiActions.showNotification({
+					status: 'error',
+					title: 'Error!',
+					message: 'Sending cart data failed!',
+				})
+			);
+		});
+	}, [cart, dispatch]);
 
 	return (
-		<Layout>
-			{cartIsVisible && <Cart />}
-			<Products />
-		</Layout>
+		<Fragment>
+			{notification && (
+				<Notification
+					status={notification.status}
+					title={notification.title}
+					message={notification.message}
+				/>
+			)}
+			<Layout>
+				{cartIsVisible && <Cart />}
+				<Products />
+			</Layout>
+		</Fragment>
 	);
 }
 
--- redux-cart/src/store/ui-slice.js
+++ redux-cart/src/store/ui-slice.js
@@ -1,6 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialUIState = { showCart: false };
+const initialUIState = { showCart: false, notification: null };
 
 const uiSlice = createSlice({
 	name: 'ui',
@@ -8,6 +8,13 @@ const uiSlice = createSlice({
 	reducers: {
 		toggleCart(state) {
 			state.showCart = !state.showCart;
+		},
+		showNotification(state, action) {
+			state.notification = {
+				status: action.payload.status,
+				title: action.payload.title,
+				message: action.payload.message,
+			};
 		},
 	},
 });
