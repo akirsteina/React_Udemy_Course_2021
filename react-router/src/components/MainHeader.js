f9c15a9c73fac696473264342cee1b669f4d3fbd
akirsteina <anna.kirsteina@gmail.com>
2021-11-22 09:46:29 +0200

    add activeClassName

--- react-router/src/components/MainHeader.js
+++ react-router/src/components/MainHeader.js
@@ -1,14 +1,19 @@
 import { NavLink } from 'react-router-dom';
+import classes from '../Assets/MainHeader.module.css';
 const MainHeader = () => {
 	return (
-		<header>
+		<header className={classes.header}>
 			<nav>
 				<ul>
 					<li>
-						<NavLink to='/welcome'>Home</NavLink>
+						<NavLink activeClassName={classes.active} to='/welcome'>
+							Home
+						</NavLink>
 					</li>
 					<li>
-						<NavLink to='/products'>Products</NavLink>
+						<NavLink activeClassName={classes.active} to='/products'>
+							Products
+						</NavLink>
 					</li>
 				</ul>
 			</nav>
