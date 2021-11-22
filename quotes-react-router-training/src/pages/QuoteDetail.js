21a98eef516c7077fa8ca0721ecc3e23165106b6
akirsteina <anna.kirsteina@gmail.com>
2021-11-22 15:18:14 +0200

    sending and getting quote data

--- quotes-react-router-training/src/pages/QuoteDetail.js
+++ quotes-react-router-training/src/pages/QuoteDetail.js
@@ -1,28 +1,57 @@
 import { Fragment } from 'react';
+import { useEffect } from 'react';
 import { useParams, Route, Link, useRouteMatch } from 'react-router-dom';
+import useHttp from '../hooks/use-http';
+import { getSingleQuote } from '../lib/api';
 
 import Comments from '../components/comments/Comments';
 import HighlightedQuote from '../components/quotes/HighlightedQuote';
+import LoadingSpinner from '../components/UI/LoadingSpinner';
 
-const DUMMY_DATA = [
-	{ id: 'q1', author: 'Anna', text: 'All is well!' },
-	{ id: 'q2', author: 'Janis', text: 'Not all those who wander are lost' },
-	{ id: 'q3', author: 'Marta', text: 'Always' },
-];
+// const DUMMY_DATA = [
+// 	{ id: 'q1', author: 'Anna', text: 'All is well!' },
+// 	{ id: 'q2', author: 'Janis', text: 'Not all those who wander are lost' },
+// 	{ id: 'q3', author: 'Marta', text: 'Always' },
+// ];
 
 const QuoteDetail = () => {
 	const params = useParams();
 	const match = useRouteMatch();
 
-	const quote = DUMMY_DATA.find((quote) => quote.id === params.quoteId);
+	const { quoteId } = params;
 
-	if (!quote) {
+	const {
+		sendRequest,
+		status,
+		data: loadedquote,
+		error,
+	} = useHttp(getSingleQuote, true);
+
+	useEffect(() => {
+		sendRequest(quoteId);
+	}, [sendRequest, quoteId]);
+
+	if (status === 'pending') {
+		return (
+			<div className='centered'>
+				<LoadingSpinner />
+			</div>
+		);
+	}
+
+	if (error) {
+		return <p className='centered'>{error}</p>;
+	}
+
+	// const quote = loadedquote.find((quote) => quote.id === params.quoteId);
+
+	if (!loadedquote.text) {
 		return <div>No quote found!</div>;
 	}
 
 	return (
 		<Fragment>
-			<HighlightedQuote text={quote.text} author={quote.author} />
+			<HighlightedQuote text={loadedquote.text} author={loadedquote.author} />
 			<Route exact path={match.path}>
 				<div className='centered'>
 					<Link className='btn--flat' to={`${match.url}/comments`}>
--- quotes-react-router-training/src/pages/Quotes.js
+++ quotes-react-router-training/src/pages/Quotes.js
@@ -1,19 +1,48 @@
+import { useEffect } from 'react/cjs/react.development';
 import { Fragment } from 'react/cjs/react.production.min';
-import QuoteList from '../components/quotes/QuoteList';
 
-const DUMMY_DATA = [
-	{ id: 'q1', author: 'Anna', text: 'All is well!' },
-	{ id: 'q2', author: 'Janis', text: 'Not all those who wander are lost' },
-	{ id: 'q3', author: 'Marta', text: 'Always' },
-];
+import useHttp from '../hooks/use-http';
+import { getAllQuotes } from '../lib/api';
+
+import QuoteList from '../components/quotes/QuoteList';
+import LoadingSpinner from '../components/UI/LoadingSpinner';
+import NoQuotesFound from '../components/quotes/NoQuotesFound';
+
+// const DUMMY_DATA = [
+// 	{ id: 'q1', author: 'Anna', text: 'All is well!' },
+// 	{ id: 'q2', author: 'Janis', text: 'Not all those who wander are lost' },
+// 	{ id: 'q3', author: 'Marta', text: 'Always' },
+// ];
 
 const Quotes = () => {
-	return (
-		<Fragment>
-			<h1>All quotes</h1>
-			<QuoteList quotes={DUMMY_DATA} />
-		</Fragment>
-	);
+	const {
+		sendRequest,
+		status,
+		data: loadedQuotes,
+		error,
+	} = useHttp(getAllQuotes, true);
+
+	useEffect(() => {
+		sendRequest();
+	}, [sendRequest]);
+
+	if (status === 'pending') {
+		return (
+			<div className='centered'>
+				<LoadingSpinner />
+			</div>
+		);
+	}
+
+	if (error) {
+		return <div className='centered focused'>{error}</div>;
+	}
+
+	if (status === 'completed' && (!loadedQuotes || loadedQuotes.length === 0)) {
+		return <NoQuotesFound />;
+	}
+
+	return <QuoteList quotes={loadedQuotes} />;
 };
 
 export default Quotes;
