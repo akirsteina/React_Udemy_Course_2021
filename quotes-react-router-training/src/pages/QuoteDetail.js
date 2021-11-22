ebca9696a1da6fff6807fbd13911155ff4e6c868
akirsteina <anna.kirsteina@gmail.com>
2021-11-22 12:30:25 +0200

    handle error

--- quotes-react-router-training/src/pages/QuoteDetail.js
+++ quotes-react-router-training/src/pages/QuoteDetail.js
@@ -2,13 +2,26 @@ import { Fragment } from 'react';
 import { useParams, Route } from 'react-router';
 
 import Comments from '../components/comments/Comments';
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
+
+const DUMMY_DATA = [
+	{ id: 'q1', author: 'Anna', text: 'All is well!' },
+	{ id: 'q2', author: 'Janis', text: 'Not all those who wander are lost' },
+	{ id: 'q3', author: 'Marta', text: 'Always' },
+];
 
 const QuoteDetail = () => {
 	const params = useParams();
+
+	const quote = DUMMY_DATA.find((quote) => quote.id === params.quoteId);
+
+	if (!quote) {
+		return <div>No quote found!</div>;
+	}
+
 	return (
 		<Fragment>
-			<h1>Product details</h1>
-			<p>{params.quoteId}</p>
+			<HighlightedQuote text={quote.text} author={quote.author} />
 			<Route path={`/quotes/${params.quoteId}/comments`}>
 				<Comments />
 			</Route>
