9a72d63030cda9fd745962dd43a7f4cf1c4009fd
akirsteina <anna.kirsteina@gmail.com>
2021-11-22 17:38:16 +0200

    programmatic navigation / replace useHistory with useNavigate

--- update-to-react-router-v6/example2/src/pages/Products.js
+++ update-to-react-router-v6/example2/src/pages/Products.js
@@ -1,22 +1,27 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Products = () => {
-  return (
-    <section>
-      <h1>The Products Page</h1>
-      <ul>
-        <li>
-          <Link to='/products/p1'>A Book</Link>
-        </li>
-        <li>
-          <Link to='/products/p2'>A Carpet</Link>
-        </li>
-        <li>
-          <Link to='/products/p3'>An Online Course</Link>
-        </li>
-      </ul>
-    </section>
-  );
+	// const navigate = useNavigate();
+	// navigate('/welcome')
+	// navigate('/welcome', {replace:true})
+	// navigate(1)
+	// replace useHistory
+	return (
+		<section>
+			<h1>The Products Page</h1>
+			<ul>
+				<li>
+					<Link to='/products/p1'>A Book</Link>
+				</li>
+				<li>
+					<Link to='/products/p2'>A Carpet</Link>
+				</li>
+				<li>
+					<Link to='/products/p3'>An Online Course</Link>
+				</li>
+			</ul>
+		</section>
+	);
 };
 
 export default Products;
