bcde8893b5edab021e968fd50b1450dd16124ebf
akirsteina <anna.kirsteina@gmail.com>
2021-11-22 14:38:27 +0200

    set dynamic pathnames

--- quotes-react-router-training/src/pages/QuoteDetail.js
+++ quotes-react-router-training/src/pages/QuoteDetail.js
@@ -1,5 +1,5 @@
 import { Fragment } from 'react';
-import { useParams, Route, Link } from 'react-router-dom';
+import { useParams, Route, Link, useRouteMatch } from 'react-router-dom';
 
 import Comments from '../components/comments/Comments';
 import HighlightedQuote from '../components/quotes/HighlightedQuote';
@@ -12,6 +12,7 @@ const DUMMY_DATA = [
 
 const QuoteDetail = () => {
 	const params = useParams();
+	const match = useRouteMatch();
 
 	const quote = DUMMY_DATA.find((quote) => quote.id === params.quoteId);
 
@@ -22,14 +23,14 @@ const QuoteDetail = () => {
 	return (
 		<Fragment>
 			<HighlightedQuote text={quote.text} author={quote.author} />
-			<Route exact path={`/quotes/${params.quoteId}`}>
+			<Route exact path={match.path}>
 				<div className='centered'>
-					<Link className='btn--flat' to={`/quotes/${params.quoteId}/comments`}>
+					<Link className='btn--flat' to={`${match.url}/comments`}>
 						Load comments
 					</Link>
 				</div>
 			</Route>
-			<Route path={`/quotes/${params.quoteId}/comments`}>
+			<Route path={`${match.path}/comments`}>
 				<Comments />
 			</Route>
 		</Fragment>
