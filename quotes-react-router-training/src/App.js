e23ba6978aea6ca255701039156654179a82a26e
akirsteina <anna.kirsteina@gmail.com>
2021-11-26 16:49:15 +0200

    add lazyloading

--- quotes-react-router-training/src/App.js
+++ quotes-react-router-training/src/App.js
@@ -1,3 +1,4 @@
+import React, { Suspense } from 'react';
 import {
 	BrowserRouter as Router,
 	Route,
@@ -6,31 +7,44 @@ import {
 } from 'react-router-dom';
 import Layout from './components/layout/Layout';
 import Quotes from './pages/Quotes';
-import QuoteDetail from './pages/QuoteDetail';
-import AddNewQuote from './pages/AddNewQuote';
-import NotFound from './pages/NotFound';
+// import QuoteDetail from './pages/QuoteDetail';
+// import AddNewQuote from './pages/AddNewQuote';
+// import NotFound from './pages/NotFound';
+import LoadingSpinner from './components/UI/LoadingSpinner';
+
+const AddNewQuote = React.lazy(() => import('./pages/AddNewQuote'));
+const QuoteDetail = React.lazy(() => import('./pages/QuoteDetail'));
+const NotFound = React.lazy(() => import('./pages/NotFound'));
 
 function App() {
 	return (
 		<Router>
 			<Layout>
-				<Switch>
-					<Route exact path='/'>
-						<Redirect to='/quotes' />
-					</Route>
-					<Route exact path='/quotes'>
-						<Quotes />
-					</Route>
-					<Route path='/quotes/:quoteId'>
-						<QuoteDetail />
-					</Route>
-					<Route path='/new-quote'>
-						<AddNewQuote />
-					</Route>
-					<Route path='*'>
-						<NotFound />
-					</Route>
-				</Switch>
+				<Suspense
+					fallback={
+						<div className='centered'>
+							<LoadingSpinner />
+						</div>
+					}
+				>
+					<Switch>
+						<Route exact path='/'>
+							<Redirect to='/quotes' />
+						</Route>
+						<Route exact path='/quotes'>
+							<Quotes />
+						</Route>
+						<Route path='/quotes/:quoteId'>
+							<QuoteDetail />
+						</Route>
+						<Route path='/new-quote'>
+							<AddNewQuote />
+						</Route>
+						<Route path='*'>
+							<NotFound />
+						</Route>
+					</Switch>
+				</Suspense>
 			</Layout>
 		</Router>
 	);
