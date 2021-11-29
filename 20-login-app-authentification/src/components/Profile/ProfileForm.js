bffa6ecefd5cce72e2c06f90b4a614ccade1a541
akirsteina <anna.kirsteina@gmail.com>
2021-11-29 15:36:41 +0200

    add change-password functionality with firebase api

--- 20-login-app-authentification/src/components/Profile/ProfileForm.js
+++ 20-login-app-authentification/src/components/Profile/ProfileForm.js
@@ -1,17 +1,51 @@
+import { useRef, useContext } from 'react';
+import AuthContext from '../../store/auth-context';
 import classes from './ProfileForm.module.css';
 
+const API_KEY = process.env.REACT_APP_API_KEY;
+
 const ProfileForm = () => {
-  return (
-    <form className={classes.form}>
-      <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
-      </div>
-      <div className={classes.action}>
-        <button>Change Password</button>
-      </div>
-    </form>
-  );
-}
+	const newPasswordInputRef = useRef();
+	const authCtx = useContext(AuthContext);
+	const submitHandler = (event) => {
+		event.preventDefault();
+		const enteredNewPassword = newPasswordInputRef.current.value;
+		// add validation
+		fetch(
+			`https://identitytoolkit.googleapis.com/v1/accounts:update?key=${API_KEY}
+    `,
+			{
+				method: 'POST',
+				body: JSON.stringify({
+					idToken: authCtx.token,
+					password: enteredNewPassword,
+					returnSecureToken: false,
+				}),
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			}
+		).then((res) => {
+			// assumption - always succeeds!
+		});
+	};
+
+	return (
+		<form className={classes.form} onSubmit={submitHandler}>
+			<div className={classes.control}>
+				<label htmlFor='new-password'>New Password</label>
+				<input
+					type='password'
+					id='new-password'
+					minLength='7'
+					ref={newPasswordInputRef}
+				/>
+			</div>
+			<div className={classes.action}>
+				<button>Change Password</button>
+			</div>
+		</form>
+	);
+};
 
 export default ProfileForm;
