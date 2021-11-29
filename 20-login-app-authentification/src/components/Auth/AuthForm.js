f454ad093b7001d25e9cf2335e3204a74eb5582c
akirsteina <anna.kirsteina@gmail.com>
2021-11-29 15:16:46 +0200

    add useContext to login and conditionally render navbar list items

--- 20-login-app-authentification/src/components/Auth/AuthForm.js
+++ 20-login-app-authentification/src/components/Auth/AuthForm.js
@@ -1,4 +1,5 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, useContext } from 'react';
+import AuthContext from '../../store/auth-context';
 
 import classes from './AuthForm.module.css';
 
@@ -9,6 +10,8 @@ const AuthForm = () => {
 	const [isLoading, setIsLoading] = useState(false);
 	const emailInputRef = useRef();
 	const passwordInputRef = useRef();
+
+	const authCtx = useContext(AuthContext);
 
 	const switchAuthModeHandler = () => {
 		setIsLogin((prevState) => !prevState);
@@ -57,7 +60,8 @@ const AuthForm = () => {
 			})
 
 			.then((data) => {
-				console.log(data);
+				// console.log(data);
+				authCtx.login(data.idToken);
 			})
 			.catch((err) => {
 				alert(err.message);
