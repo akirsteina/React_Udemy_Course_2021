4e1949165f895786b0117052f89f63f5f662f7e3
akirsteina <anna.kirsteina@gmail.com>
2021-12-01 12:50:39 +0200

    update token expiration

--- 20-login-app-authentification/src/store/auth-context.js
+++ 20-login-app-authentification/src/store/auth-context.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 let logoutTimer;
 
@@ -16,29 +16,60 @@ const calculcateRemainingTime = (expirationTime) => {
 	return remainingDuration;
 };
 
+const retrieveStoredToken = () => {
+	const storedToken = localStorage.getItem('token');
+	const storedExpirationDate = localStorage.getItem('expirationTime');
+	const remainingTime = calculcateRemainingTime(storedExpirationDate);
+	if (remainingTime <= 60000) {
+		localStorage.removeItem('token');
+		localStorage.removeItem('expirationTime');
+		return null;
+	}
+
+	return { token: storedToken, duration: remainingTime };
+};
+
 export const AuthContextProvider = (props) => {
-	const initialToken = localStorage.getItem('token');
+	const tokenData = retrieveStoredToken();
+	// const initialToken = localStorage.getItem('token');
+	let initialToken;
+	if (tokenData) {
+		//only truthy if we have a token, otherwise null
+		initialToken = tokenData.token;
+	}
 	const [token, setToken] = useState(initialToken);
 
 	// !! converts to a boolean value, e.g., string is true
 	const userIsLoggedIn = !!token;
 
-	const logoutHandler = () => {
+	// add useCallback as it is a dependency of useEffect
+	// to prevent infinite loops
+	const logoutHandler = useCallback(() => {
 		setToken(null);
 		localStorage.removeItem('token');
+		localStorage.removeItem('expirationTime');
 		if (logoutTimer) {
 			clearTimeout(logoutTimer);
 		}
-	};
+	}, []);
 
 	const loginHandler = (token, expirationTime) => {
 		setToken(token);
-		localStorage.setItem('token', token);
+		localStorage.setItem('token', token); // store the token for using again when reloading the page
+		localStorage.setItem('expirationTime', expirationTime); // has to be string // set every time on login
 
 		const remainingTime = calculcateRemainingTime(expirationTime);
 
 		logoutTimer = setTimeout(logoutHandler, remainingTime); // logoutHandler is executed when the timer expires (remaining time in milliseconds)
 	};
+
+	useEffect(() => {
+		// sets the logout handler to the remaining time if we automatically logged in the user (on page refresh)
+		if (tokenData) {
+			console.log(tokenData.duration);
+			logoutTimer = setTimeout(logoutHandler, tokenData.duration);
+		}
+	}, [tokenData, logoutHandler]);
 
 	const contextValue = {
 		token,
