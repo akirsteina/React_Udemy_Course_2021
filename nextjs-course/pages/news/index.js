36e87879f319db30c89af0c8ea4ae83cfb5331c0
akirsteina <anna.kirsteina@gmail.com>
2021-12-02 09:00:29 +0200

    add navigation with a links

--- nextjs-course/pages/news/index.js
+++ nextjs-course/pages/news/index.js
@@ -1,7 +1,24 @@
 // ourdomain.com/news
 
+import { Fragment } from 'react';
+
 const NewsPage = () => {
-	return <h1>The news page</h1>;
+	return (
+		<Fragment>
+			<h1>The news page</h1>
+			<ul>
+				<li>
+					<a href='/news/news-are-cool'>News are cool</a>
+				</li>
+				<li>
+					<a href='/news/tuesday'>Today is Thursday</a>
+				</li>
+				<li>
+					<a href='news/xmas'>Christmas is coming</a>
+				</li>
+			</ul>
+		</Fragment>
+	);
 };
 
 export default NewsPage;
