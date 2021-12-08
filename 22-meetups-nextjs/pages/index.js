e46c4a639e7f93c12e55fcfa5b29c680d315aa6e
akirsteina <anna.kirsteina@gmail.com>
2021-12-08 08:57:02 +0200

    add getServerSideProps to fetch data on every request

--- 22-meetups-nextjs/pages/index.js
+++ 22-meetups-nextjs/pages/index.js
@@ -37,10 +37,25 @@ const HomePage = (props) => {
 // has to be called getStaticProps
 // load data before the component function is executed
 // this code is not executed on client side and is only executed during the build
-export const getStaticProps = async () => {
-	// fetch data
+// export const getStaticProps = async () => {
+// 	// fetch data
 
-	// always need to return object
+// 	// always need to return object
+// 	return {
+// 		props: {
+// 			meetups: DUMMY_MEETUPS,
+// 		},
+// 		revalidate: 10,
+// 	};
+// };
+
+// does not run during build, but always on the server after the deployment
+// never runs in the client, only on server (can also use credentials)
+// runs for every incoming request
+export const getServerSideProps = async (context) => {
+	const req = context.req;
+	const res = context.res;
+	// fetch data from API
 	return {
 		props: {
 			meetups: DUMMY_MEETUPS,
