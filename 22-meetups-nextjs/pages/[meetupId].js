7fa759a55c5b0235d0718a82fc3b4f5ddb09ce2f
akirsteina <anna.kirsteina@gmail.com>
2021-12-08 15:39:59 +0200

    fetch meetup details

--- 22-meetups-nextjs/pages/[meetupId].js
+++ 22-meetups-nextjs/pages/[meetupId].js
@@ -1,42 +1,53 @@
 // ourdomain.com/[meetupId]
-
+import { MongoClient, ObjectId } from 'mongodb';
 import MeetupDetail from '../components/meetups/MeetupDetail';
 
-const MeetupDetails = () => {
+const REACT_APP_DB_URL = process.env.REACT_APP_DB_URL;
+
+const MeetupDetails = (props) => {
 	return (
 		<MeetupDetail
-			image='https://images.freeimages.com/images/large-previews/ce3/city-1570944.jpg'
-			id='m1'
-			title='title'
-			address='address'
-			description='description'
+			image={props.meetupData.image}
+			title={props.meetupData.title}
+			address={props.meetupData.address}
+			description={props.meetupData.description}
 		/>
 	);
 };
 
 export const getStaticPaths = async () => {
+	const client = await MongoClient.connect(`${REACT_APP_DB_URL}`);
+	const db = client.db();
+	const meetupsCollection = db.collection('meetups');
+	// all object with no filter criteria
+	// only include ids, but no other values
+	const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray();
+	client.close();
 	return {
 		// false - paths contain all supported meetup id values
 		// true - tries to create page dynamically
 		// e.g. some (most visited) pages are created on build, but others - dynamically
 		fallback: false,
-		paths: [
-			{
-				params: {
-					meetupId: 'm1',
-				},
-			},
-			{
-				params: {
-					meetupId: 'm2',
-				},
-			},
-			{
-				params: {
-					meetupId: 'm3',
-				},
-			},
-		],
+		paths: meetups.map((meetup) => ({
+			params: { meetupId: meetup._id.toString() },
+		})),
+		// paths: [
+		// 	{
+		// 		params: {
+		// 			meetupId: 'm1',
+		// 		},
+		// 	},
+		// 	{
+		// 		params: {
+		// 			meetupId: 'm2',
+		// 		},
+		// 	},
+		// 	{
+		// 		params: {
+		// 			meetupId: 'm3',
+		// 		},
+		// 	},
+		// ],
 	};
 };
 
@@ -45,18 +56,32 @@ export const getStaticProps = async (context) => {
 
 	const meetupId = context.params.meetupId;
 
-	console.log(meetupId);
+	const client = await MongoClient.connect(`${REACT_APP_DB_URL}`);
+	const db = client.db();
+	const meetupsCollection = db.collection('meetups');
+
+	const selectedMeetup = await meetupsCollection.findOne({
+		_id: ObjectId(meetupId),
+	});
+	client.close();
 
 	return {
 		props: {
 			meetupData: {
-				image:
-					'https://images.freeimages.com/images/large-previews/ce3/city-1570944.jpg',
-				id: meetupId,
-				title: 'title',
-				address: 'address',
-				description: 'description',
+				id: selectedMeetup._id.toString(),
+				title: selectedMeetup.title,
+				address: selectedMeetup.address,
+				description: selectedMeetup.description,
+				image: selectedMeetup.image,
 			},
+			// {
+			// 	image:
+			// 		'https://images.freeimages.com/images/large-previews/ce3/city-1570944.jpg',
+			// 	id: meetupId,
+			// 	title: 'title',
+			// 	address: 'address',
+			// 	description: 'description',
+			// },
 		},
 	};
 };
